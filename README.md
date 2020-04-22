d1a1fa13d98e848f1530b42cc18a4fef2b0b1b59
chuanbozhu_ajk <chuanbozhu_ajk@58.com>
2020-04-22 12:29:14 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+### 0.运行界面
+![UI](https://raw.githubusercontent.com/sufadi/AccessibilityServiceMonitor/master/device-2018-08-22-114621.png)
+
+### 1. 相关博客
+
+请问AccessibilityService如何识别webview内容(20180822解决)
+- https://blog.csdn.net/su749520/article/details/79726071
+
+AccessibilityService 进行蚂蚁森林能量收集 demo
+- https://blog.csdn.net/su749520/article/details/81943782
+
+Android辅助工具助手-keep 自动点赞器
+- https://blog.csdn.net/su749520/article/details/79760792
+
+AccessibilityService 手机联通营业厅自动签到器
+- https://blog.csdn.net/su749520/article/details/82153456
+
+AccessibilityService 微信运动自动点赞器
+- https://blog.csdn.net/su749520/article/details/82225580
---
+++ app/src/main/java/com/fadi/forestautoget/service/AlipayForestMonitor.java
@@ -0,0 +1,114 @@
+package com.fadi.forestautoget.service;
+
+import android.content.Context;
+import android. content.Intent;
+import android.util.Log;
+import android.view.accessibility.AccessibilityNodeInfo;
+
+import com.fadi.forestautoget.util.Config;
+
+import java.util.List;
+
+public class AlipayForestMonitor {
+
+
+    /**
+     * 启动支付宝界面
+     * adb shell am start com.eg.android.AlipayGphone/com.eg.android.AlipayGphone.AlipayLogin
+     */
+    public static void startAlipay(Context mContext) {
+        Intent intent = new Intent();
+        intent.setPackage("com.anjuke.android.app");
+        intent.setClassName("com.anjuke.android.app", "com.anjuke.android.app.common.activity.MainTabPageActivity");
+        mContext.startActivity(intent);
+    }
+
+    /**
+     * 自动点击进入蚂蚁森林界面
+     */
+    public static void enterForestUI(AccessibilityNodeInfo nodeInfo) {
+        Log.d(Config.TAG, "enterForestUI ");
+        if (nodeInfo != null) {
+            // 找到界面中蚂蚁森林的文字
+            List<AccessibilityNodeInfo> list = nodeInfo.findAccessibilityNodeInfosByText("找小区");
+
+            if (list == null) {
+                Log.d(Config.TAG, "enterForestUI finding no");
+                return;
+            } else {
+                Log.d(Config.TAG, "enterForestUI finding yes");
+            }
+
+            for (AccessibilityNodeInfo item : list) {
+                /**
+                 *  蚂蚁森林本身不可点击，但是他的父控件可以点击
+                 */
+                AccessibilityNodeInfo parent = item.getParent();
+                if (null != parent && parent.isClickable()) {
+                    parent.performAction(AccessibilityNodeInfo.ACTION_CLICK);
+                    Log.d(Config.TAG, "item = " + item.toString() + ", parent click = " + parent.toString());
+                    break;
+                }
+            }
+        }
+    }
+
+    public static void policy(AccessibilityNodeInfo nodeInfo, String packageName, String className) {
+        /**
+         * 蚂蚁森林界面
+         */
+        if (packageName.equals("com.anjuke.android.app") ) {
+
+            if (nodeInfo != null) {
+                for (int i = 0; i < nodeInfo.getChildCount(); i++) {
+                    AccessibilityNodeInfo child =  nodeInfo.getChild(i);
+                    if ("com.uc.webview.export.WebView".equals(child.getClassName())) {
+                        Log.d(Config.TAG, "找到蚂蚁森林的 webView count = " + child.getChildCount());
+
+                        findEveryViewNode(child);
+                        break;
+                    }
+                }
+            } else {
+                Log.d(Config.TAG, "alipayPolicy = null");
+            }
+        }
+
+    }
+
+    public static void findEveryViewNode(AccessibilityNodeInfo node) {
+        if (null != node && node.getChildCount() > 0) {
+            for (int i = 0; i < node.getChildCount(); i++) {
+                AccessibilityNodeInfo child =  node.getChild(i);
+                // 有时 child 为空
+                if (child == null) {
+                    continue;
+                }
+
+                //Log.d(TAG, "findEveryViewNode = " + child.toString());
+
+                String className = child.getClassName().toString();
+                if ("android.widget.Button".equals(className)) {
+                    Log.d(Config.TAG, "Button 的节点数据 text = " + child.getText() + ", descript = " + child.getContentDescription() + ", className = " + child.getClassName() + ", resId = " + child.getViewIdResourceName());
+
+                    boolean isClickable = child.isClickable();
+                    boolean isResIdNull = child.getViewIdResourceName() == null ? true:false;
+
+                    /**
+                     * 好友的能量不能收取，因为支付宝在onTouch事件中return true,导致不会触发OnClick方法
+                     *
+                     * 但是支付宝中的蚂蚁森林可以收取自己的能量
+                     */
+                    if ( isClickable && isResIdNull) {
+                        child.performAction(AccessibilityNodeInfo.ACTION_CLICK);
+                        Log.d(Config.TAG, "能量球 成功点击");
+                    }
+                }
+
+                // 递归调用
+                findEveryViewNode(child);
+            }
+        }
+    }
+
+}
